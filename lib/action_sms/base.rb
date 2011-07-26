17edf36b0f355efb0d89ba5e3aad5a30a9a1791f
Adman65 <Adman1965@gmail.com>
2011-07-26 15:16:26 +0300

    Subclass action mailer for inheritable config!

--- lib/action_sms/base.rb
+++ lib/action_sms/base.rb
@@ -1,128 +1,14 @@
 require 'action_mailer'
 
 module ActionSms
-  class Base
-    include ::ActionMailer::AdvAttrAccessor
-
-    if Object.const_defined?(:ActionController)
-      include ::ActionController::UrlWriter
-    end
-
-    private_class_method :new #:nodoc:
-
-    class_inheritable_accessor :view_paths
-    self.view_paths = []
-
-    cattr_accessor :logger
-
-    @@raise_delivery_errors = true
-    cattr_accessor :raise_delivery_errors
-
-    @@perform_deliveries = true
-    cattr_accessor :perform_deliveries
-
+  class Base < ActionMailer::Base
     @@delivery_method = :http
-    cattr_accessor :delivery_method
-
-    @@deliveries = []
-    cattr_accessor :deliveries
 
     adv_attr_accessor :to
-
     adv_attr_accessor :from
-
     adv_attr_accessor :message
 
-    # Specify the template name to use for current message. This is the "base"
-    # template name, without the extension or directory, and may be used to
-    # have multiple mailer methods share the same template.
-    adv_attr_accessor :template
-
-    # Override the mailer name, which defaults to an inflected version of the
-    # mailer's class name. If you want to use a template in a non-standard
-    # location, you can use this to specify that location.
-    def mailer_name(value = nil)
-      if value
-        self.mailer_name = value
-      else
-        self.class.mailer_name
-      end
-    end
-
-    def mailer_name=(value)
-      self.class.mailer_name = value
-    end
-
-    # The sms object instance referenced by this mailer.
     attr_reader :sms
-    attr_reader :template_name, :default_template_name, :action_name
-
-    class << self
-      attr_writer :mailer_name
-
-      def mailer_name
-        @mailer_name ||= name.underscore
-      end
-
-      # Default to ActionMailer's url options
-      # This library uses the same configuration as ActionMailer
-      def default_url_options
-        ActionMailer::Base.default_url_options
-      end
-
-      # for ActionView compatibility
-      alias_method :controller_name, :mailer_name
-      alias_method :controller_path, :mailer_name
-
-      def respond_to?(method_symbol, include_private = false) #:nodoc:
-        matches_dynamic_method?(method_symbol) || super
-      end
-
-      def method_missing(method_symbol, *parameters) #:nodoc:
-        if match = matches_dynamic_method?(method_symbol)
-          case match[1]
-            when 'create'  then new(match[2], *parameters).sms
-            when 'deliver' then new(match[2], *parameters).deliver!
-            when 'new'     then nil
-            else super
-          end
-        else
-          super
-        end
-      end
-
-      # Deliver the given sms object directly. This can be used to deliver
-      # a preconstructed sms object, like:
-      #
-      #   email = MyMailer.create_some_mail(parameters)
-      #   email.set_some_obscure_header "frobnicate"
-      #   MyMailer.deliver(email)
-      def deliver(sms)
-        new.deliver!(sms)
-      end
-
-      def template_root
-        self.view_paths && self.view_paths.first
-      end
-
-      def template_root=(root)
-        self.view_paths = ActionView::Base.process_view_paths(root)
-      end
-
-      private
-      def matches_dynamic_method?(method_name) #:nodoc:
-        method_name = method_name.to_s
-        /^(create|deliver)_([_a-z]\w*)/.match(method_name) || /^(new)$/.match(method_name)
-      end
-    end
-
-    # Instantiate a new mailer object. If +method_name+ is not +nil+, the mailer
-    # will be initialized according to the named method. If not, the mailer will
-    # remain uninitialized (useful when you only need to invoke the "receive"
-    # method, for instance).
-    def initialize(method_name=nil, *parameters) #:nodoc:
-      create!(method_name, *parameters) if method_name
-    end
 
     # Initialize the mailer via the given +method_name+. The body will be
     # rendered and a new SMS::Message object created.
@@ -193,6 +79,8 @@ module ActionSms
 
     def create_sms_message
       @sms = Message.new(:to => @to, :from => @from, :text => @message)
+      @mail = @sms # for ActionMailer
+      @sms
     end
 
     def perform_delivery_http(sms)
@@ -202,9 +90,5 @@ module ActionSms
     def perform_delivery_test(sms)
       deliveries << sms
     end
-  end
-
-  class Base
-    include ::ActionMailer::Helpers
   end
 end
